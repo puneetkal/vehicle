0df09d15362c75cf57a53f8bc839b6a08ef0f594
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-04 17:33:29 +0100

    Extract Head into data type

--- vehicle-syntax/src/Vehicle/Syntax/AST/Value.hs
+++ vehicle-syntax/src/Vehicle/Syntax/AST/Value.hs
@@ -12,41 +12,34 @@ import Vehicle.Syntax.AST.Binder (GenericBinder)
 import Vehicle.Syntax.AST.Expr (Expr, UniverseLevel)
 import Vehicle.Syntax.AST.Meta (MetaID)
 import Vehicle.Syntax.AST.Name (Identifier)
+import Vehicle.Syntax.AST.Provenance (Provenance)
 import Vehicle.Syntax.External.Abs (Name)
+
+data Head var builtin
+  = Builtin builtin
+  | BoundVar var
+  | FreeVar Identifier
+  | MetaID
+  deriving (Eq, Show, Generic)
 
 data Value binder var builtin env
   = -- | A universe, used to type types.
     Universe
-      -- Provenance
+      Provenance
       UniverseLevel
-  | -- | Application of one term to another.
-    AppBuiltin
-      -- Provenance
-      builtin -- Builtin function.
-      [Value binder var builtin env] -- Arguments.
-  | -- | Variables that are bound locally by other expressions
-    AppBoundVar
-      -- Provenance
-      var -- Variable name.
-      (Spine binder var builtin env)
-  | -- | Variables that refer to other declarations
-    AppFreeVar
-      -- Provenance
-      Identifier -- Declaration name
-      (Spine binder var builtin env)
-  | -- | Unsolved meta variables.
-    AppMeta
-      -- Provenance
-      MetaID -- Meta variable number.
-      (Spine binder var builtin env)
+  | -- | Application of an unreduced head to a spine.
+    App
+      Provenance
+      (Head var builtin) -- Head.
+      (Spine binder var builtin env) -- Arguments.
   | -- | Dependent product (subsumes both functions and universal quantification).
     Pi
-      -- Provenance
+      Provenance
       (Binder binder var builtin env) -- The bound name
       (Value binder var builtin env) -- (Dependent) result type.
   | -- | Lambda expressions (i.e. anonymous functions).
     Lam
-      -- Provenance
+      Provenance
       (Binder binder var builtin env) -- Bound expression name.
       (env var (Value binder var builtin env)) -- Environment captured by the closure.
       (Expr binder var builtin) -- Expression body.
@@ -67,5 +60,7 @@ type Spine binder var builtin env = [Arg binder var builtin env]
 deriving instance (Eq binder, Eq var, Eq builtin, Eq (env var (Value binder var builtin env))) => Eq (Value binder var builtin env)
 
 deriving instance (Show binder, Show var, Show builtin, Show (env var (Value binder var builtin env))) => Show (Value binder var builtin env)
+
+instance (Serialize var, Serialize builtin) => Serialize (Head var builtin)
 
 instance (Serialize binder, Serialize var, Serialize builtin, Serialize (env var (Value binder var builtin env))) => Serialize (Value binder var builtin env)
